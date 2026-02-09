505210be38297527b0c44ba553c4502142c39bdc
Your Name <krishna.yudha.agastya@gmail.com>
2026-02-09 11:04:03 +0800

    Refactor Quickbuy tests to use asset locators and improve button state assertions

---
+++ tests/Indodax/data/quickbuy_assets.ts
@@ -0,0 +1,18 @@
+import { Page, Locator } from '@playwright/test';
+
+export class QuickbuyAssets {
+  readonly page: Page;
+  readonly Button100: Locator;
+  readonly Button250: Locator;
+  readonly Button500: Locator;
+  readonly Button1000: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    // Store buttons as locators
+    this.Button100 =  page.getByRole('button', { name: '100.000' })
+    this.Button250 =  page.getByRole('button', { name: '250.000' })
+    this.Button500 =  page.getByRole('button', { name: '500.000' })
+    this.Button1000 =  page.getByRole('button', { name: '1.000.000' })
+  }
+}
