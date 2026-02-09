0d9d3b91e36b83747846b33d7c39be313ee3de9a
Your Name <krishna.yudha.agastya@gmail.com>
2026-02-09 11:04:03 +0800

    Refactor Quickbuy tests to use asset locators and improve button state assertions

--- tests/Indodax/QuickBuy.spec.ts
+++ tests/Indodax/QuickBuy.spec.ts
@@ -1,21 +1,22 @@
 import { test, expect, Page } from '@playwright/test';
-
+import { QuickbuyAssets as MyPage, QuickbuyAssets } from '../Indodax/data/quickbuy_assets';
 const url = 'https://indodax.com/quick_buy';
 
 test('Verify amount is shown correctly', async ({ page }) => {
+  const quickbuy = new MyPage(page);
   await page.goto(url);
   await page.getByRole('button', { name: 'Coba Sekarang' }).click();
   await page.getByRole('button', { name: 'Lewati' }).click();
-  await page.getByRole('button', { name: '100.000' }).click();
+  await quickbuy.Button100.click();
   const amountshow = page.getByRole('textbox', { name: 'Nominal Pembelian' });
   await expect(amountshow).toBeVisible();
   await expect(amountshow).toBeDisabled();
   await expect(amountshow).toHaveValue('100.000');
-  await page.getByRole('button', { name: '250.000' }).click();
+  await quickbuy.Button250.click();
   await expect(amountshow).toHaveValue('250.000');
-  await page.getByRole('button', { name: '500.000' }).click();
+  await quickbuy.Button500.click();
   await expect(amountshow).toHaveValue('500.000');
-  await page.getByRole('button', { name: '1.000.000' }).click();
+  await quickbuy.Button1000. click();
   await expect(amountshow).toHaveValue('1.000.000');  
 });
 
