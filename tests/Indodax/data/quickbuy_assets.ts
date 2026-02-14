38ca5498f42bb46d3b16dd3049e8e4024a554338
Your Name <krishna.yudha.agastya@gmail.com>
2026-02-14 22:08:42 +0800

    add new Locator

--- tests/Indodax/data/quickbuy_assets.ts
+++ tests/Indodax/data/quickbuy_assets.ts
@@ -6,6 +6,9 @@ export class QuickbuyAssets {
   readonly Button250: Locator;
   readonly Button500: Locator;
   readonly Button1000: Locator;
+  readonly ButtonCobaSekarang: Locator;
+  readonly ButtonLewati: Locator;
+  readonly EstRecieved: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -14,5 +17,8 @@ export class QuickbuyAssets {
     this.Button250 =  page.getByRole('button', { name: '250.000' })
     this.Button500 =  page.getByRole('button', { name: '500.000' })
     this.Button1000 =  page.getByRole('button', { name: '1.000.000' })
+    this.ButtonCobaSekarang = page.getByRole('button', { name: 'Coba Sekarang' });
+    this.ButtonLewati = page.getByRole('button', { name: 'Lewati' });
+    this.EstRecieved = page.getByRole('textbox', { name: 'Total Aset' });
   }
 }
