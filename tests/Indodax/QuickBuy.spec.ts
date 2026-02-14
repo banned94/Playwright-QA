6180732f6166186761c9f3417465684f54207ac1
Your Name <krishna.yudha.agastya@gmail.com>
2026-02-14 22:08:10 +0800

    Refactor Code , reducing flakiness

--- tests/Indodax/QuickBuy.spec.ts
+++ tests/Indodax/QuickBuy.spec.ts
@@ -1,17 +1,25 @@
-import { test, expect, Page } from '@playwright/test';
-import { QuickbuyAssets as MyPage, QuickbuyAssets } from './data/quickbuy_assets';
+import { test, expect, Page, Locator } from '@playwright/test';
+import { QuickbuyAssets as MyPage } from './data/quickbuy_assets';
+import { parse } from 'node:path';
 const url = 'https://indodax.com/quick_buy';
 
+let quickbuy: MyPage;
+let estRecieved: Locator;
+
 test.beforeEach(async ({ page }) => {
- 
+  quickbuy = new MyPage(page);
+  estRecieved = quickbuy.EstRecieved;
   await page.goto(url);
-  await page.waitForTimeout(6000);
-  const cobaBtn = page.getByRole('button', { name: 'Coba Sekarang' });
-  if (await cobaBtn.isVisible()) {
+});
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(url);
+  const quickbuy = new MyPage(page);
+  const cobaBtn = quickbuy.ButtonCobaSekarang;
+  if (await cobaBtn.isVisible) {
     await cobaBtn.click();
   }
-
-  const lewatiBtn = page.getByRole('button', { name: 'Lewati' });
+  const lewatiBtn = quickbuy.ButtonLewati;
   if (await lewatiBtn.isVisible()) {
     await lewatiBtn.click();
   } 
@@ -19,8 +27,6 @@ test.beforeEach(async ({ page }) => {
 
 test('Verify amount is shown correctly', async ({ page }) => {
   const quickbuy = new MyPage(page);
-  await page.goto(url); 
-  await quickbuy.Button100.waitFor({state: 'visible'})
   await quickbuy.Button100.click();
   const amountshow = page.getByRole('textbox', { name: 'Nominal Pembelian' });
   await expect(amountshow).toBeVisible();
@@ -30,7 +36,7 @@ test('Verify amount is shown correctly', async ({ page }) => {
   await expect(amountshow).toHaveValue('250.000');
   await quickbuy.Button500.click();
   await expect(amountshow).toHaveValue('500.000');
-  await quickbuy.Button1000. click();
+  await quickbuy.Button1000.click();
   await expect(amountshow).toHaveValue('1.000.000');  
 });
 
@@ -42,18 +48,56 @@ async function getUsdtPrice(page: Page) {
   return Number(match![1].replace('.', ''));
 }
 
-test('Calculate USDT price ', async ({ page }) => {
-  await page.waitForTimeout(5000);
+test('Calculate USDT price 1.000.000 ', async ({ page }) => {
   const getPrice = await getUsdtPrice(page);
-  console.log('FIRST PRICE:', getPrice);
-
-  const calculatedPrice =Number((1000000 / getPrice) * 100) / 100;
-  const formatted = calculatedPrice.toFixed(8).replace('.', ',');
+  const estRecieved = new MyPage(page).EstRecieved;
+  console.log('FIRST PRICE:', getPrice); 
+  const calculatedPrice = Number(((1000000 / getPrice) * 100) / 100);
+  const formatted = (Math.floor(calculatedPrice * 1e8) / 1e8)
+  .toFixed(8)
+  .replace('.', ',');
   console.log('CALCULATED PRICE:', formatted);
 
   await page.getByRole('button', { name: '1.000.000' }).click();
-  expect(await page.getByRole('textbox', { name: 'Total Aset' }).inputValue()).toBe(formatted);
-  console.log('DISPLAYED PRICE:', formatted);
+  expect(await estRecieved.inputValue()).toBe(formatted);
+  console.log('DISPLAYED PRICE:', await estRecieved.inputValue());
+} );
 
-
+test('Calculate USDT price 500.000 ', async ({ page }) => {
+  const getPrice = await getUsdtPrice(page);
+  console.log('FIRST PRICE:', getPrice); 
+  const calculatedPrice = Number(((500000 / getPrice) * 100) / 100);
+  const formatted = (Math.floor(calculatedPrice * 1e8) / 1e8)
+  .toFixed(8)
+  .replace('.', ',');
+  console.log('CALCULATED PRICE:', formatted);
+  await page.getByRole('button', { name: '500.000' }).click();
+  expect(await estRecieved.inputValue()).toBe(formatted);
+  console.log('DISPLAYED PRICE:', await estRecieved.inputValue());
 });
+
+test('Calculate USDT price 250.000 ', async ({ page }) => {
+  const getPrice = await getUsdtPrice(page);
+  console.log('FIRST PRICE:', getPrice); 
+  const calculatedPrice = Number(((250000 / getPrice) * 100) / 100);
+  const formatted = (Math.floor(calculatedPrice * 1e8) / 1e8)
+  .toFixed(8)
+  .replace('.', ',');
+  console.log('CALCULATED PRICE:', formatted);
+  await page.getByRole('button', { name: '250.000' }).click();
+  expect(await estRecieved.inputValue()).toBe(formatted);
+  console.log('DISPLAYED PRICE:', await estRecieved.inputValue());
+} );
+
+test('Calculate USDT price 100.000 ', async ({ page }) => {
+  const getPrice = await getUsdtPrice(page);
+  console.log('FIRST PRICE:', getPrice);  
+  const calculatedPrice = Number(((100000 / getPrice) * 100) / 100);
+  const formatted = (Math.floor(calculatedPrice * 1e8) / 1e8)
+  .toFixed(8)
+  .replace('.', ',');
+  console.log('CALCULATED PRICE:', formatted);
+  await page.getByRole('button', { name: '100.000' }).click();
+  expect(await estRecieved.inputValue()).toBe(formatted);
+  console.log('DISPLAYED PRICE:', await estRecieved.inputValue());
+} );
