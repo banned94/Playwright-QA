097189b91ae4c5a1706904408b2d00c7e16f2346
Your Name <krishna.yudha.agastya@gmail.com>
2026-02-15 12:36:20 +0800

    Reupload

--- QuickBuy.spec.ts
+++ QuickBuy.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect, Page, Locator } from '@playwright/test';
-import { QuickbuyAssets as MyPage } from './tests/Indodax/data/quickbuy_assets';
-import { parse } from 'node:path';
+import { QuickbuyAssets as MyPage } from './data/quickbuy_assets';
 const url = 'https://indodax.com/quick_buy';
 
 let quickbuy: MyPage;
