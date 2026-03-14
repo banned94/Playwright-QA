6732a6c57e0eef6db67acf5adee9cc5ce9f0e0bd
Krishna <krishna.yudha.agastya@gmail.com>
2026-03-14 15:01:27 +0800

    move tests from tests/tests to tests

--- tests/SauceDemo/saucedemo_problem.spec.ts
+++ tests/SauceDemo/saucedemo_problem.spec.ts
@@ -1,14 +1,15 @@
 import { test, expect } from '@playwright/test';
+import {SauceDemoAssets as MyPage} from './data/saucedemo_assets';
+import { users } from './data/user_data';
+
 
 test('saucedemo login and add to cart', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-
-    // Fill in username and password fields     
-        await page.getByPlaceholder('Username').fill('problem_user');
-        await page.getByPlaceholder('Password').fill('secret_sauce');
-            // Click the login button   
-                await page.getByRole('button', { name: 'Login' }).click();
-                    // Expects the products image to be broken     
-                const image = page.locator('img[src="/static/media/sl-404.168b1cce10384b857a6f.jpg"]');
-                await expect(image).toHaveCount(6);
+const saucedemo = new MyPage(page);
+await page.goto(saucedemo.url);  
+await saucedemo.UsernameField.fill(users.standard.username);
+await saucedemo.PasswordField.fill(users.standard.password);
+await saucedemo.LoginButton.click();
+// Expects the products image to be broken     
+const image = page.locator('img[src="/static/media/sl-404.168b1cce10384b857a6f.jpg"]');
+await expect(image).toHaveCount(6);
 });
